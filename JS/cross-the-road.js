010cc78000b62313cb65ce3bef670fd6ba2d0703
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-10-14 20:29:25 +0300

    adding JS for cross the road game

--- JS/cross-the-road.js
+++ JS/cross-the-road.js
@@ -7,10 +7,120 @@ document.addEventListener('DOMContentLoaded',()=> {
     let replayBtn =document.querySelector('#replay');
     let leftBtn =document.querySelector(".left");
     let rightBtn =document.querySelector(".right");
+    let controlUp =document.querySelector(".control .up");
+    let controlLeft =document.querySelector(".control .left");
+    let controlRight =document.querySelector(".control .right");
+    let controlDown =document.querySelector(".control .down");
+    let manId=202;
+    squares[manId].classList.add("man");
     let width = 15 ;
+    let cars=[[0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0] ];
     let direction =1;
     let result =0 ;
-    let gameTimer = setInterval (moveInvaders,400);
+    let rowsStart=[15,30,45,60,90,105,120,150,165,180];
+    let rowEnd =rowsStart.map((e)=>e+width-1);
+    let rowMoveingDirction = [1,1,0,0,1,1,1,0,0,0] ; // 1:right and 0:left
+    let gameTimer = setInterval (moveCars,400);
+    showCars();
+    function showCars () {
+        for (let row=0;row<rowsStart.length;row++){
+            for (let rand=0;rand<7;rand++){
+                cars[row][rand]=Math.floor(Math.random()*(width-1)+rowsStart[row]);
+                squares[cars[row][rand]].classList.add("car");
+            }
+        }
+    }
+    function moveCars () {
+        for (let row=0;row<rowsStart.length;row++){
+            for(let car=0;car<7;car++){
+                squares[cars[row][car]].classList.remove("car");
+                if(rowMoveingDirction[row]===0){ //left
+                    if (cars[row][car]>rowsStart[row]){
+                        cars[row][car] -=1;
+                    }else {
+                        cars[row][car] +=14;
+                    }
+                }else { // right
+                    if (cars[row][car]<rowEnd[row]){
+                        cars[row][car] +=1;
+                    }else {
+                        cars[row][car] -=14;
+                    }
+                }
+                squares[cars[row][car]].classList.add("car");
+            }
+        }
+        // lose
+        if (squares[manId].classList.contains("car")){
+            gameArea.classList.add("end");
+            resultBox.classList.add("end");
+            resultBoxMass.textContent="you lose!"
+        }
+    }
+    document.addEventListener("keydown", function(event) {
+        squares[manId].classList.remove("man");
+        if(event.key=="ArrowDown"){
+            if (manId<(15*14)){
+                manId +=15;
+            }
+        }else if(event.key=="ArrowUp"){
+            if (manId>14){
+                manId -=15;
+            }
+        }else if(event.key=="ArrowLeft"){
+            if (manId%15>0){
+                manId -=1;
+            }
+        }else if(event.key=="ArrowRight"){
+            if (manId%15<14){
+                manId +=1;
+            }
+        }
+        squares[manId].classList.add("man");
+        // win 
+        if (manId<15){
+            gameArea.classList.add("end");
+            resultBox.classList.add("end");
+            resultBoxMass.textContent="you win!"
+        }
+    });
+    controlUp.onclick = function() {
+        squares[manId].classList.remove("man");
+        if (manId>14){
+            manId -=15;
+        }
+        squares[manId].classList.add("man");
+    }
+    controlLeft.onclick = function() {
+        squares[manId].classList.remove("man");
+        if (manId%15>0){
+            manId -=1;
+        }
+        squares[manId].classList.add("man");
+    }
+    controlRight.onclick = function() {
+        squares[manId].classList.remove("man");
+        if (manId%15<14){
+            manId +=1;
+        }
+        squares[manId].classList.add("man");
+    }
+    controlDown.onclick = function() {
+        squares[manId].classList.remove("man");
+        if (manId<(15*14)){
+            manId +=15;
+        }
+        squares[manId].classList.add("man");
+    }
     replayBtn.addEventListener("click", function() {
         window.location = window.location.href;
     });
